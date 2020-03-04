574206a0c652e263fc4e2e595dd23f347f7b7207
Monica Giambitto <monica.giambitto@gmail.com>
2020-03-05 00:12:45 +0100

    base display book page test

--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe "Book browsing", :type => :request do
 
   context "when a book exists in the library" do
     it "displays book's details" do
+      get "/books/1"
 
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Book title")
     end
   end
 
