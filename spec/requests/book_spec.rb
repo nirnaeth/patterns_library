3086008ad4a9127f06154e3b44e5b15a47eaba68
Monica Giambitto <monica.giambitto@gmail.com>
2020-03-04 23:31:51 +0100

    flesh behavior for book detail page

---
+++ spec/requests/book_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe "Book browsing", :type => :request do
+
+  context "when a book exists in the library" do
+    it "displays book's details" do
+
+    end
+  end
+
+  context "when a book doesn't exist in the library" do
+    it "presents a not found page" do
+
+    end
+  end
+end
+
